6a4b6b1d8ac5cd1e7089a99e9264c0f9e7f63bc5
Vitaly Skorobreshuk <vitaly.skorobreshuk@mev.com>
2015-08-07 14:39:24 +0300

    Added base project

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '5.0' 
+
+pod 'MBProgressHUD', '~> 0.6'
+pod 'Reachability', '~> 3.1.0'
+pod 'CKRefreshControl'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CKRefreshControl
+#define COCOAPODS_POD_AVAILABLE_CKRefreshControl
+#define COCOAPODS_VERSION_MAJOR_CKRefreshControl 1
+#define COCOAPODS_VERSION_MINOR_CKRefreshControl 1
+#define COCOAPODS_VERSION_PATCH_CKRefreshControl 1
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 1
+
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 1
+#define COCOAPODS_VERSION_PATCH_Reachability 1
+
